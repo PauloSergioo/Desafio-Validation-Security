b87328327df21116ac63521ef15517a0a8807123
Paulo Sergio <paulosergioilha38@gmail.com>
2023-06-29 12:35:26 -0300

    TDD from EventControllerIT finished

--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -1,7 +1,40 @@
 package com.devsuperior.demo.services;
 
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.entities.Event;
+import com.devsuperior.demo.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EventService {
+
+    @Autowired
+    EventRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<EventDTO> findAllPaged(Pageable pageable) {
+        Page<Event> list = repository.findAll(pageable);
+        return list.map(EventDTO::new);
+    }
+
+    @Transactional
+    public EventDTO insert(EventDTO dto) {
+        Event entity = new Event();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new EventDTO(entity);
+    }
+
+    private void copyDtoToEntity(EventDTO dto, Event entity) {
+
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setUrl(dto.getUrl());
+        entity.setCity(new City(dto.getCityId(), null));
+    }
 }
