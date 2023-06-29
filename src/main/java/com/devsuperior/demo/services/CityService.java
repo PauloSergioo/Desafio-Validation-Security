7234e8cfa6c41c3eb7a348c261c3dae0ee6f9736
Paulo Sergio <paulosergioilha38@gmail.com>
2023-06-28 22:20:43 -0300

    TDD from CityControllerIT finished

--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -1,7 +1,33 @@
 package com.devsuperior.demo.services;
 
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CityService {
+
+    @Autowired
+    CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll(Sort name) {
+        List<City> list = repository.findAll(Sort.by("name"));
+        return list.stream().map(CityDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+        City entity = new City();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CityDTO(entity);
+    }
 }
